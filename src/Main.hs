afce399a14602260898dd0b230c7be01db39174f
Daniel Slocombe <ds4314@ic.ac.uk>
2017-06-11 01:26:46 +0100

    Colouring of suggestion, completing based on dir

--- src/Main.hs
+++ src/Main.hs
@@ -2,15 +2,32 @@ module Main where
 
 import Trie
 import System.Directory
+import System.Console.ANSI
+
+stripQuotes :: String -> String
+stripQuotes = filter (\x -> x /= '"')
 
 main :: IO ()
 main = do
   currentDir <- getCurrentDirectory
   putStrLn $ show currentDir
   files <- listDirectory currentDir
-  putStrLn $ concatMap show files
-  let tries = foldr (insertCW) [] (fmap show files)
-      x = fmap fromCharWeight $ lookupCW "\"s" tries
-  -- putStrLn $ show tries
-  putStrLn x
-  putStrLn "hello world"
+  putStrLn $ concatMap (((++)"\n") . stripQuotes . show) files
+  let tries = buildTries files
+  completeIO tries
+
+
+buildTries :: [FilePath] -> [Trie CharWeight]
+buildTries files = foldr insertCW [] (fmap (stripQuotes . show) files)
+
+complete :: String -> [Trie CharWeight] -> String
+complete s ts = fmap fromCharWeight $ lookupCW s ts
+
+completeIO :: [Trie CharWeight] -> IO ()
+completeIO tries = do
+  putStr ">>> "
+  prefix <- getLine
+  putStr ('\n':prefix)
+  setSGR [SetColor Foreground Vivid Red]
+  putStrLn $ drop (length prefix) (complete prefix tries)
+  setSGR [Reset]
